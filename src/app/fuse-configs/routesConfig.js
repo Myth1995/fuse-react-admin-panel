25ee8f1941728500ff6f6c855f760775a4c98a15
myth19950803 <myth19950803@gmail.com>
2021-11-14 21:03:26 +0300

    Delete unused resource and code.

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,22 +1,22 @@
 import FuseUtils from '@fuse/utils';
 import appsConfigs from 'app/main/apps/appsConfigs';
-import authRoleExamplesConfigs from 'app/main/auth/authRoleExamplesConfigs';
+// import authRoleExamplesConfigs from 'app/main/auth/authRoleExamplesConfigs';
 import CallbackConfig from 'app/main/callback/CallbackConfig';
-import DocumentationConfig from 'app/main/documentation/DocumentationConfig';
+// import DocumentationConfig from 'app/main/documentation/DocumentationConfig';
 import LoginConfig from 'app/main/login/LoginConfig';
 import LogoutConfig from 'app/main/logout/LogoutConfig';
-import pagesConfigs from 'app/main/pages/pagesConfigs';
+// import pagesConfigs from 'app/main/pages/pagesConfigs';
 import RegisterConfig from 'app/main/register/RegisterConfig';
-import UserInterfaceConfig from 'app/main/user-interface/UserInterfaceConfig';
+// import UserInterfaceConfig from 'app/main/user-interface/UserInterfaceConfig';
 import { Redirect } from 'react-router-dom';
 import FuseLoading from '@fuse/core/FuseLoading';
 
 const routeConfigs = [
   ...appsConfigs,
-  ...pagesConfigs,
-  ...authRoleExamplesConfigs,
-  UserInterfaceConfig,
-  DocumentationConfig,
+  // ...pagesConfigs,
+  // ...authRoleExamplesConfigs,
+  // UserInterfaceConfig,
+  // DocumentationConfig,
   LogoutConfig,
   LoginConfig,
   RegisterConfig,
